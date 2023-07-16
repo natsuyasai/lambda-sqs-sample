a2218d8ea665b3ac1bfa857ebc646e2de6c98ff7
natsuyasai <yasai038tomato@gmail.com>
2023-07-16 12:44:17 +0900

    キー名修正

--- sqs_recive.py
+++ sqs_recive.py
@@ -4,7 +4,7 @@ def lambda_handler(event, context):
     batch_item_failures = []
     sqs_batch_response = {}
     for record in event["Records"]:
-        print(event['Records'][0]['messageAttributes']['TestData']['StringValue'])
+        print(event['Records'][0]['messageAttributes']['TestData']['stringValue'])
         batch_item_failures.append({"itemIdentifier": record['messageId']})
     
     sqs_batch_response["batchItemFailures"] = batch_item_failures
