bd1dc8cbfcf9704c170364fece35a90ab04ec8fb
natsuyasai <yasai038tomato@gmail.com>
2023-07-16 12:39:35 +0900

    受信処理追加

---
+++ sqs_recive.py
@@ -0,0 +1,15 @@
+
+def lambda_handler(event, context):
+    print(event['Records'])
+    batch_item_failures = []
+    sqs_batch_response = {}
+    for record in event["Records"]:
+        print(event['Records'][0]['messageAttributes']['TestData']['StringValue'])
+        batch_item_failures.append({"itemIdentifier": record['messageId']})
+    
+    sqs_batch_response["batchItemFailures"] = batch_item_failures
+    return sqs_batch_response
+
+
+if __name__ == '__main__':
+    lambda_handler(None, None)
